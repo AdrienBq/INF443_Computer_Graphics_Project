562c5fa92b9b3a838b17860cb4b2292d4c5c3d0a
Erwan Umlil <erwan.umlil@polytechnique.edu>
2021-05-11 07:36:23 +0000

    Files and structure created
    (cherry picked from commit 33299c99124dfeaf1be83dfa8a9df018cc58729f)

---
+++ src/items/boat.cpp
@@ -0,0 +1,171 @@
+#include "vcl/vcl.hpp"
+#include <iostream>
+
+
+using namespace vcl;
+
+struct scene_environment
+{
+	camera_around_center camera;
+	mat4 projection;
+	vec3 light;
+};
+scene_environment scene;
+
+struct gui_parameters {
+	bool display_frame = false;
+	bool display_surface = true;
+	bool display_wireframe = false;
+};
+
+struct user_interaction_parameters {
+	vec2 mouse_prev;
+	timer_fps fps_record;
+	mesh_drawable global_frame;
+	gui_parameters gui;
+	bool cursor_on_gui;
+};
+user_interaction_parameters user;
+
+
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos);
+void window_size_callback(GLFWwindow* window, int width, int height);
+
+
+void initialize_data();
+void display_interface();
+void display_frame();
+
+timer_interval timer;
+
+
+int main(int, char* argv[])
+{
+	std::cout << "Run " << argv[0] << std::endl;
+
+	int const width = 1280, height = 1024;
+	GLFWwindow* window = create_window(width, height);
+	window_size_callback(window, width, height);
+	std::cout << opengl_info_display() << std::endl;;
+
+	imgui_init(window);
+	glfwSetCursorPosCallback(window, mouse_move_callback);
+	glfwSetWindowSizeCallback(window, window_size_callback);
+
+	std::cout << "Initialize data ..." << std::endl;
+	initialize_data();
+
+	std::cout << "Start animation loop ..." << std::endl;
+	user.fps_record.start();
+	glEnable(GL_DEPTH_TEST);
+	while (!glfwWindowShouldClose(window))
+	{
+		scene.light = scene.camera.position();
+		user.fps_record.update();
+
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		imgui_create_frame();
+		if (user.fps_record.event) {
+			std::string const title = "VCL Display - " + str(user.fps_record.fps) + " fps";
+			glfwSetWindowTitle(window, title.c_str());
+		}
+
+		ImGui::Begin("GUI", NULL, ImGuiWindowFlags_AlwaysAutoResize);
+		user.cursor_on_gui = ImGui::IsAnyWindowFocused();
+
+		if (user.gui.display_frame) draw(user.global_frame, scene);
+
+		display_interface();
+		display_frame();
+
+
+		ImGui::End();
+		imgui_render_frame(window);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	imgui_cleanup();
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
+
+
+
+void initialize_data()
+{
+	// Basic setups of shaders and camera
+	GLuint const shader_mesh = opengl_create_shader_program(opengl_shader_preset("mesh_vertex"), opengl_shader_preset("mesh_fragment"));
+	mesh_drawable::default_shader = shader_mesh;
+	mesh_drawable::default_texture = opengl_texture_to_gpu(image_raw{ 1,1,image_color_type::rgba,{255,255,255,255} });
+
+	user.global_frame = mesh_drawable(mesh_primitive_frame());
+	user.gui.display_frame = false;
+	scene.camera.distance_to_center = 2.5f;
+	scene.camera.look_at({ -0.5f,2.5f,1 }, { 0,0,0 }, { 0,0,1 });
+
+}
+
+
+
+void display_frame()
+{
+	// Update the current time
+	timer.update();
+	float const t = timer.t;
+
+}
+
+
+void display_interface()
+{
+	ImGui::SliderFloat("Time", &timer.t, timer.t_min, timer.t_max);
+	ImGui::SliderFloat("Time scale", &timer.scale, 0.0f, 2.0f);
+	ImGui::Checkbox("Frame", &user.gui.display_frame);
+	ImGui::Checkbox("Surface", &user.gui.display_surface);
+	ImGui::Checkbox("Wireframe", &user.gui.display_wireframe);
+}
+
+
+void window_size_callback(GLFWwindow*, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	float const aspect = width / static_cast<float>(height);
+	float const fov = 50.0f * pi / 180.0f;
+	float const z_min = 0.1f;
+	float const z_max = 100.0f;
+	scene.projection = projection_perspective(fov, aspect, z_min, z_max);
+}
+
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	vec2 const  p1 = glfw_get_mouse_cursor(window, xpos, ypos);
+	vec2 const& p0 = user.mouse_prev;
+	glfw_state state = glfw_current_state(window);
+
+	auto& camera = scene.camera;
+	if (!user.cursor_on_gui) {
+		if (state.mouse_click_left && !state.key_ctrl)
+			scene.camera.manipulator_rotate_trackball(p0, p1);
+		if (state.mouse_click_left && state.key_ctrl)
+			camera.manipulator_translate_in_plane(p1 - p0);
+		if (state.mouse_click_right)
+			camera.manipulator_scale_distance_to_center((p1 - p0).y);
+	}
+
+	user.mouse_prev = p1;
+}
+
+void opengl_uniform(GLuint shader, scene_environment const& current_scene)
+{
+	opengl_uniform(shader, "projection", current_scene.projection);
+	opengl_uniform(shader, "view", current_scene.camera.matrix_view());
+	opengl_uniform(shader, "light", current_scene.light, false);
+}
+
+
+
