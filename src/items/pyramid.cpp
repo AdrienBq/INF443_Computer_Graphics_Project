2f10edb8fa13f4896431580cd4959acd9e1c687b
Erwan Umlil <erwan.umlil@polytechnique.edu>
2021-05-11 10:05:06 +0200

    New structure

---
+++ src/items/pyramid.cpp
@@ -0,0 +1,10 @@
+#include "pyramid.hpp"
+
+using namespace vcl;
+
+
+vcl::mesh create_pyramid(float base, float height) {
+	vcl::mesh pyramid;
+	pyramid.position.resize(5);
+	return pyramid;
+}
---
+++ src/items/pyramid.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "vcl/vcl.hpp"
+
+vcl::mesh create_pyramid(float base, float height);
